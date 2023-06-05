e15c9f82388d1fd52d6d464aac5ec6455f495060
JiaYu0220 <alice49885@gmail.com>
2023-06-05 21:24:06 +0800

    大更改

--- js/app.js
+++ js/app.js
@@ -1,3 +1,52 @@
+// ---------- 手機版 header 選單收合 ----------
+$(document).ready(() => {
+  //點擊 btn 控制選單出現、消失
+  $(".header_mobile_btn").click(() => {
+    $(".header_mobile_menu").toggleClass("show");
+  });
+});
+
+// ---------- swiper輪播動畫 ----------
+//new Swiper() 有兩個參數：
+// 第一個參數為字串，需填入欲套用輪播效果的 swiper 容器，以上述基本結構為例，swiper 容器為 .swiper
+// 第二個參數為物件，非必填，可以填入想調整的選項
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1, // 一次看幾個
+  spaceBetween: 24, // 間距(px)
+
+  // swiper 斷點設定為 mobile first
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    768: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true, // 点击分页器的指示点分页器会控制 Swiper 切换
+  },
+});
+
+// ---------- QA收合選單 ----------
+$(".question_item").click(function () {
+  $(this).toggleClass("active");
+});
+
+// ---------- toTop ----------
+$(".toTop").click(() => {
+  $("html").animate(
+    {
+      scrollTop: 0,
+    },
+    1000
+  );
+});
+
 // ---------- API ----------
 // *** 資料串接 ***
 const apiPath = "https://2023-engineer-camp.zeabur.app";
@@ -63,10 +112,94 @@ function renderWorks() {
     </div>
   </li>`;
   });
-
   list.innerHTML = works;
 }
 
+// ---------- 搜尋 ----------
+const search = document.querySelector("#search");
+search.addEventListener("keydown", (e) => {
+  //按下 enter 時
+  if (e.keyCode === 13) {
+    data.search = search.Value;
+    data.page = 1;
+    getData(data);
+  }
+});
+
+// ---------- 排序、篩選----------
+// *** 收合篩選選單-全部篩選 ***
+let tempFilterName = []; // ai 加入 [0]、type 加入 [1]，最後用 join("、") 連接
+let filterName;
+
+// 篩選選單 - 出現、消失
+$(document).ready(() => {
+  $(".tool_filter_all_btn").click(() => {
+    $(".tool_filter_all_menu").toggleClass("show");
+  });
+
+  // 篩選選單 - Ai模型
+  $(".tool_filter_all_menu_ai").click(function () {
+    // filetr-icon 變黑底白字
+    $(".tool_filter_all_btn .material-icons").addClass("icon-white");
+    // active
+    $(".tool_filter_all_menu_ai").removeClass("active");
+    $(this).addClass("active");
+    // btn文字
+    tempFilterName[0] = $(this).find(".filter_name").text().trim();
+    filterName = tempFilterName.join("、");
+    $(".tool_filter_all_btn_txt").text(filterName);
+    // console.log(filterName);
+  });
+
+  // 篩選選單 - 類型
+  $(".tool_filter_all_menu_type").click(function () {
+    // filetr-icon 變黑底白字
+    $(".tool_filter_all_btn .material-icons").addClass("icon-white");
+    // active
+    $(".tool_filter_all_menu_type").removeClass("active");
+    $(this).addClass("active");
+    // btn文字
+    tempFilterName[1] = $(this).find(".filter_name").text().trim();
+    filterName = tempFilterName.filter((x) => x !== undefined).join("、"); // 若沒有選 ai 篩選，要去除 [0] 沒有值，避免出現 "、"
+    $(".tool_filter_all_btn_txt").text(filterName);
+    // console.log(filterName);
+  });
+});
+
+// *** 切換作品類型 ***
+const filterBtns = document.querySelectorAll(".filterBtn");
+filterBtns.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    $(filterBtns).removeClass("active");
+    $(item).addClass("active");
+    if (item.textContent === "全部") {
+      data.type = "";
+    } else {
+      data.type = item.textContent;
+      console.log(data);
+    }
+    getData(data);
+  });
+});
+
+// *** 切換作品排序 ***
+const desc = document.querySelector("#desc");
+const asc = document.querySelector("#asc");
+
+// 由新到舊 -> sort = 0
+desc.addEventListener("click", () => {
+  data.sort = 0;
+  getData(data);
+});
+
+//由舊到新 -> sort = 1
+asc.addEventListener("click", () => {
+  data.sort = 1;
+  getData(data);
+});
+
+// ---------- 分頁 ----------
 // *** 切換分頁 ***
 function changePage(pagesData) {
   const pageLinks = document.querySelectorAll("a.pageLink");
@@ -109,153 +242,16 @@ function renderPages() {
   changePage(pagesData);
 }
 
-// ---------- 排序、篩選----------
-// *** 切換作品排序 ***
-const desc = document.querySelector("#desc");
-const asc = document.querySelector("#asc");
+// // *** 收合篩選選單-時間 ***
+// $(document).ready(() => {
+//   //點擊 btn 控制選單出現、消失
+//   $(".tool_filter_time_btn").click(() => {
+//     $(".tool_filter_time_menu").toggleClass("show");
+//   });
 
-// 由新到舊 -> sort = 0
-desc.addEventListener("click", (e) => {
-  e.preventDefault();
-  data.sort = 0;
-  getData(data);
-});
-
-//由舊到新 -> sort = 1
-asc.addEventListener("click", (e) => {
-  e.preventDefault();
-  data.sort = 1;
-  getData(data);
-});
-
-// *** 切換作品類型 ***
-const filterBtns = document.querySelectorAll("#filterBtn");
-filterBtns.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (item.textContent === "全部") {
-      data.type = "";
-    } else {
-      data.type = item.textContent;
-    }
-    getData(data);
-  });
-});
-
-//搜尋
-const search = document.querySelector("#search");
-search.addEventListener("keydown", (e) => {
-  //按下 enter 時
-  if (e.keyCode === 13) {
-    data.search = search.Value;
-    data.page = 1;
-    getData(data);
-  }
-});
-
-// ---------- 動畫 ----------
-// *** 輪播動畫 ***
-$(document).ready(() => {
-  let currentIndex = 0;
-
-  function showSildes(index) {
-    console.log(index);
-    // 移除class名稱
-    $(".recommend_carousel_item").removeClass("active");
-    $(".recommend_indicator_item").removeClass("active");
-
-    //eq(index).addClass("active")： 為 index = currentIndex 的物件增加class名稱
-    $(".recommend_carousel_item").eq(index).addClass("active");
-    $(".recommend_indicator_item").eq(index).addClass("active");
-
-    //card 由左到右排列，最左邊的 card 會顯示在畫面中，每個card相隔 100% 的距離
-    // css 要先把 card 之間的 gap 拿掉，否則無法對其
-    let transitionValue = -(index * 100) + "%";
-    $(".recommend_carousel_item").css(
-      "transform",
-      "translateX(" + transitionValue + ")"
-    );
-  }
-
-  $(".recommend_indicator_item").click(function () {
-    //index()返回当前指示器元素在其同级元素中的索引值（從0開始計數）
-    currentIndex = $(this).index();
-    // console.log(currentIndex);
-    showSildes(currentIndex);
-  });
-});
-
-// *** 收合篩選選單-全部篩選 ***
-let tempFilterName = []; // ai 加入 [0]、type 加入 [1]，最後用 join("、") 連接
-let filterName;
-
-// 篩選選單 - 出現、消失
-$(document).ready(() => {
-  $(".tool_filter_all_btn").click(() => {
-    $(".tool_filter_all_menu").toggleClass("show");
-  });
-  // 篩選選單 - Ai模型
-  $(".tool_filter_all_menu_ai").click(function () {
-    // filetr-icon 變黑底白字
-    $(".tool_filter_all_btn .material-icons").addClass("icon-white");
-    // active
-    $(".tool_filter_all_menu_ai").removeClass("active");
-    $(this).addClass("active");
-    // btn文字
-    tempFilterName[0] = $(this).find(".filter_name").text().trim();
-    filterName = tempFilterName.join("、");
-    $(".tool_filter_all_btn_txt").text(filterName);
-    // console.log(filterName);
-  });
-
-  // 篩選選單 - 類型
-  $(".tool_filter_all_menu_type").click(function () {
-    // filetr-icon 變黑底白字
-    $(".tool_filter_all_btn .material-icons").addClass("icon-white");
-    // active
-    $(".tool_filter_all_menu_type").removeClass("active");
-    $(this).addClass("active");
-    // btn文字
-    tempFilterName[1] = $(this).find(".filter_name").text().trim();
-    filterName = tempFilterName.filter((x) => x !== undefined).join("、"); // 若沒有選 ai 篩選，要去除 [0] 沒有值，避免出現 "、"
-    $(".tool_filter_all_btn_txt").text(filterName);
-    // console.log(filterName);
-  });
-});
-
-//切換作品類型
-$(document).ready(() => {
-  $("#filterBtn a").click(() => {
-    $("#filterBtn a").removeClass(".active");
-    $(this).addClass(".active");
-  });
-});
-
-// *** 收合篩選選單-時間 ***
-$(document).ready(() => {
-  //點擊 btn 控制選單出現、消失
-  $(".tool_filter_time_btn").click(() => {
-    $(".tool_filter_time_menu").toggleClass("show");
-  });
-
-  //點擊選單選項後切換 btn 文字，並選單消失
-  $(".new-to-old,.old-to-new").click(function () {
-    $(".tool_filter_time_btn_txt").text($(this).text());
-    $(".tool_filter_time_menu").toggleClass("show");
-  });
-});
-
-// *** QA收合選單 ***
-$(".question_item").click(function () {
-  $(this).toggleClass("active");
-});
-
-// *** toTop ***
-$(".toTop").click(() => {
-  $("html").animate(
-    {
-      scrollTop: 0,
-    },
-    1000
-  );
-});
+//   //點擊選單選項後切換 btn 文字，並選單消失
+//   $(".new-to-old,.old-to-new").click(function () {
+//     $(".tool_filter_time_btn_txt").text($(this).text());
+//     $(".tool_filter_time_menu").toggleClass("show");
+//   });
+// });
